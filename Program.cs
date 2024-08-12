7a834de11d121db9a891a411ef6d6e9a341573a8
vtcong2299 <vtcong2299@gmail.com>
2024-08-12 17:45:27 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data.Common;
+using System.Reflection.Emit;
+
+
+namespace UocChungLonNhat
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int soNguyenA;
+            int soNguyenB;
+            int ucln=1;
+           
+            Console.Write("Nhap so nguyen A: ");
+            while (true)
+            {
+                if(Int32.TryParse(Console.ReadLine(), out soNguyenA))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Nhap lai so nguyen A: ");
+                }
+            }
+            Console.Write("Nhap so nguyen B: ");
+            while (true)
+            {
+                if(Int32.TryParse(Console.ReadLine(), out soNguyenB))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write("Nhap lai so nguyen B: ");
+                }
+            }
+            int a=Math.Abs(soNguyenA);
+            int b=Math.Abs(soNguyenB);
+            if(a==0||b==0)
+            {
+                Console.WriteLine("Khong co uoc chung lon nhat");
+            }
+            while (a!=b)
+            {
+                if(a>b)
+                {
+                    if(a%b!=0)
+                    {
+                        a=a%b; 
+                        ucln=a;
+                    }
+                    else
+                    {
+                        ucln=b;
+                        break;
+                    }
+                    
+                }
+                else
+                {
+                    
+                    if(b%a!=0)
+                    {
+                        b=b%a;
+                        ucln=b;
+                    }
+                    else
+                    {
+                        ucln=a;
+                        break;
+                    }
+                }
+                               
+            }
+           
+            Console.Write("Uoc chung lon nhat cua {0} va {1} la: {2}",soNguyenA,soNguyenB, ucln);
+         
+            
+        }
+    }
+}
